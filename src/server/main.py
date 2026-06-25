2f6a115319cc9cd26afa584d0aa05c512bd8c1aa
Maxgamertyper1 <maxa5302@gmail.com>
2026-06-25 17:30:58 -0600

    Dynamic Version

--- src/server/main.py
+++ src/server/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from datetime import datetime, timezone
 
 app = FastAPI()
+VERISON = "0.0.1"
 
 # To start the server run "fastapi dev ./src/server/main.py"
 @app.get("/")
 def read_root():
     return {
         "sourceURL": "https://github.com/maxgamertyper/C4GMinesCybersecurity/tree/main",
-        "version": "1.0.0",
+        "version": VERISON,
         "environment": "production"
         }
 
