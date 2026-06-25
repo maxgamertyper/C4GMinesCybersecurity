cbbed1f3ac3730a29dad5d2282d50b02fd580b6a
Maxgamertyper1 <maxa5302@gmail.com>
2026-06-25 17:29:28 -0600

    base endpoint

--- src/server/main.py
+++ src/server/main.py
@@ -4,7 +4,13 @@ from datetime import datetime, timezone
 app = FastAPI()
 
 # To start the server run "fastapi dev ./src/server/main.py"
-
+@app.get("/")
+def read_root():
+    return {
+        "sourceURL": "https://github.com/maxgamertyper/C4GMinesCybersecurity/tree/main",
+        "version": "1.0.0",
+        "environment": "production"
+        }
 
 @app.get("/health")
 def read_root():
