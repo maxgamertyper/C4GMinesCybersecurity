210e97817efc3099f5ae700ee72a8a7e63ec7cfb
Maxgamertyper1 <maxa5302@gmail.com>
2026-06-25 23:03:15 -0600

    Default path test

---
+++ src/server/tests/requestTests.py
@@ -0,0 +1,36 @@
+import requests
+from rich import print
+
+BASE_SERVER_IP_ADDRESS = "http://127.0.0.1:8000"
+
+def test_warning(text:str):
+    print("[bold yellow][Warning!][/bold yellow]: " + text)
+
+def test_alert(text:str):
+    print("[bold red][Alert!][/bold red]: " + text)
+
+def test_information(text:str):
+    print("[bold][Information][/bold]: " + text)
+
+def test_succeed(text:str):
+    print("[bold green][Success!][/bold green]: " + text)
+
+
+def default_path_test():
+    response = None
+
+    try:
+        response = requests.get(BASE_SERVER_IP_ADDRESS)
+    except requests.exceptions.ConnectionError:
+        test_alert("Server is not responding, you may need to start it with \"fastapi dev\"")
+        return
+    
+    responseDict = response.json()
+
+    if responseDict["environment"] != "production" or responseDict["sourceURL"] != "https://github.com/maxgamertyper/C4GMinesCybersecurity/tree/main" or responseDict["version"] == None:
+        test_warning("Server didn't respond as expected in base path!")
+        test_information(response.content)
+    else:
+        test_succeed("Default gateway responded correctly")
+
+default_path_test()
