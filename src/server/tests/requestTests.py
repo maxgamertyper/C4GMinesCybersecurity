7b1624fb36f5c0f27d5ce8cf1d53859fd32bb822
Maxgamertyper1 <maxa5302@gmail.com>
2026-06-25 23:06:59 -0600

    Warning print fix

--- src/server/tests/requestTests.py
+++ src/server/tests/requestTests.py
@@ -29,8 +29,20 @@ def default_path_test():
 
     if responseDict["environment"] != "production" or responseDict["sourceURL"] != "https://github.com/maxgamertyper/C4GMinesCybersecurity/tree/main" or responseDict["version"] == None:
         test_warning("Server didn't respond as expected in base path!")
-        test_information(response.content)
+        test_information("Response: \n" + str(responseDict))
     else:
         test_succeed("Default gateway responded correctly")
+
+
+def feedback_test():
+    TEST_FEEDBACK = "Hello, this is a feedback test"
+
+    response = requests.post(BASE_SERVER_IP_ADDRESS + "/feedback",json={"feedback":TEST_FEEDBACK})
+
+    if response.status_code != 204:
+        test_alert("Feedback gateway unresponsive")
+    else: 
+        test_succeed("Feedback gateway responded correctly")
+
 
 default_path_test()
