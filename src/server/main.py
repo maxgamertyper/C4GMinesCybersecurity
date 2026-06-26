73b5efa7efc90b745428ec85b018188150b191c5
angellahao <angella9654@gmail.com>
2026-06-26 14:12:55 -0600

    Added CORS and temporary analyze and feedback endpoints

--- src/server/main.py
+++ src/server/main.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 from datetime import datetime, timezone
 
 app = FastAPI()
-VERISON = "0.0.1"
+VERSION = "0.0.1"
+
+# Allows the chrome extension/frontend to communicate w/ the backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
 
 # To start the server run "fastapi dev ./src/server/main.py"
 @app.get("/")
 def read_root():
     return {
         "sourceURL": "https://github.com/maxgamertyper/C4GMinesCybersecurity/tree/main",
-        "version": VERISON,
+        "version": VERSION,
         "environment": "production"
         }
 
@@ -26,3 +37,29 @@ def read_root():
     return {
         "implementedTests": []
         }
+
+# TEMPORARY analyze endpoint. this will call the actual phishing tests and scoring system later
+@app.post("/analyze")
+def analyze_email(email: dict) :
+    return {
+        "score": 50,
+        "threatLevel": "Likely phishing",
+        "reason": "It looks suspicious.",
+        "passedTests": [],
+        "failedTests": [
+            {
+                "testName": "placeholder_test",
+                "details": "Backend received the email data successfully."
+            }
+        ],
+        "receivedEmail": email
+
+    }
+
+# TEMPORARY feedback endpoint. This will store user feedback in the database later
+@app.post("/feedback")
+def submit_feedback(feedback: dict) :
+    return{
+        "status": "recieved",
+        "feedback": feedback
+    }
