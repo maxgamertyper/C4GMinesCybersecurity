f72ae99419b3adae2c02acfcf3c28ee230211152
Maxgamertyper1 <maxa5302@gmail.com>
2026-07-09 10:23:13 -0600

    initial setup

--- src/server/fileHandler.py
+++ src/server/fileHandler.py
@@ -5,6 +5,11 @@ import os
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 FEEDBACK_FILE_PATH = os.path.join(CURRENT_DIR, "feedback.txt")
 
+
+
+
+
+#silly text writing
 def write_feedback(feedback:str):
 
     with open(FEEDBACK_FILE_PATH,"a+") as feedbackFile:
@@ -13,3 +18,78 @@ def write_feedback(feedback:str):
         feedbackFile.close()
     
     return
+
+
+#sql
+def create_if_nonesxistent(conn, cursor):
+    cursor.execute("PRAGMA foreign_keys = ON;") # enforce foreign key rules
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS suspicious_actors (
+        actorID INTEGER PRIMARY KEY AUTOINCREMENT,
+        actorName TEXT,
+        actorTYPE TEXT,
+        suspicionLevel INTEGER,
+        firstSeen DATETIME DEFAULT CURRENT_TIMESTAMP,
+        lastSeen DATETIME DEFAULT CURRENT_TIMESTAMP,
+        occurrences INTEGER,
+        manualConfirmation BOOLEAN
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS emails_table (
+        emailID INTEGER PRIMARY KEY AUTOINCREMENT,
+        senderName TEXT,
+        senderEmail TEXT,
+        subject TEXT,
+        body TEXT
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS attachments_table (
+        attachmentID INTEGER PRIMARY KEY AUTOINCREMENT,
+        emailID INTEGER,
+        attachmentName TEXT,
+        attachmentExtension TEXT,
+        
+        FOREIGN KEY (emailID) REFERENCES emails_table(emailID)
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS analysis_feedback (
+        submissionID INTEGER PRIMARY KEY AUTOINCREMENT,
+        emailID INTEGER,
+        submissionTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+        userAccuracyResponse BOOLEAN,
+        serverVersion TEXT,
+        phishingScore INTEGER,
+        phishingType TEXT,
+        
+        FOREIGN KEY (emailID) REFERENCES emails_table(emailID)
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tests_table (
+        testID INTEGER PRIMARY KEY AUTOINCREMENT,
+        submissionID INTEGER,
+        testScore INTEGER,
+        testWeight INTEGER,
+        testName TEXT,
+        testSuccessful BOOLEAN,
+        
+        FOREIGN KEY (submissionID) REFERENCES analysis_feedback(submissionID)
+    )
+    """)
+
+
+    # Trigger is used to autmatically update the lastSeen value when the item is updated
+    cursor.execute("""
+    CREATE TRIGGER IF NOT EXISTS update_actor_last_seen
+    AFTER UPDATE ON suspicious_actors
+    BEGIN
+        UPDATE suspicious_actors 
+        SET lastSeen = CURRENT_TIMESTAMP 
+        WHERE actorID = NEW.actorID;
+    END;
+    """)
+    conn.commit()
+
--- src/server/main.py
+++ src/server/main.py
@@ -1,10 +1,33 @@
-from fastapi import FastAPI, status, Body
+from fastapi import FastAPI, status, Body, Request
 from fastapi.middleware.cors import CORSMiddleware
-from server import utility
-from server import fileHandler
+from server import utility, fileHandler
+from contextlib import asynccontextmanager
+import os
+import sqlite3
 
-app = FastAPI()
+
 VERSION = "0.0.1"
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_FILE_PATH = os.path.join(CURRENT_DIR, "sushiSQL.db")
+
+@asynccontextmanager
+async def server_wrapper(app: FastAPI):
+    #run before the server starts accepting requests
+    conn = sqlite3.connect(DATABASE_FILE_PATH, check_same_thread=False)
+    cursor = conn.cursor()
+
+    fileHandler.create_if_nonexistent(conn, cursor)
+
+    #store them in fastAPI
+    app.state.db_conn = conn
+    app.state.db_cursor = cursor
+
+    yield # let the server do stuff
+
+    conn.close() # close after the server shuts off
+
+app = FastAPI(lifespan=server_wrapper)
 
 # Allow the chrome extension/frontend to connect to the backend
 app.add_middleware(
@@ -71,8 +94,30 @@ def post_feedback(feedback:str = Body(embed=True)): # embeds the feedback field 
 
 
 @app.post("/accuracy", status_code=status.HTTP_200_OK)
-def post_accuracy(payload: dict):
+def post_accuracy(payload: dict, request: Request):
 
+    {
+        "body":"Hello, this is an accuracy placeholder body",
+        "sender": "sender@placeholder.com",
+        "subject": "Placeholder Subject",
+        "accuracy": True, # pressed yes on the extension
+        "attachments": ["hi.exe","suspicious.xlsx"],
+        "analysisReturn": {
+            "score": 50,
+            "threatLevel": "likely phishing",
+            "reason": "This is a placeholder.",
+            "passedTests": [],
+            "failedTests": [
+                {
+                    "testName": "placeholder_test",
+                    "testScore": 50,
+                    "testWeight": 1,
+                    "testPassed": False,
+                    "testDetails": "Placeholder"
+                }
+                ],
+            }
+        }
     # store to database
 
     return {
